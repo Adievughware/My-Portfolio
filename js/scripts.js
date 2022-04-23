255f25bc6828fb705c07f243904df7db5e573585
Adievughware Oghenetega <oghenetegaadievughware@gmail.com>
2022-04-23 07:44:58 +0100

    changed css/styles.css index.html js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,3 @@
-// TYPING TEXTS
-
-const TypeWriiter = function(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = wait;
-    this.type();
-    this.isDeleting = false;
-}
-
-// Type Method
-TypeWriiter.prototype.type = function(){
-    // Current index of word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of current words
-    const fullTxt = this.words[current];
-
-    // Check if deleting
-    if(this.isDeleting){
-        //Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else{
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    // Insert txt into Element
-    this.txtElement.innerHTML = `<span class ="txt">${this.txt}</span>`;
-
-    // Initial Type Speed
-    let typeSpeed = 100;
-
-    if(this.isDeleting){
-        typeSpeed /= 2;
-    }
-
-    // If word is complete
-    if(!this.isDeleting && this.txt === fullTxt){
-        // Make pause at end
-        typeSpeed = this.wait;
-        // Set delete to true
-        this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === ''){
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing 
-        typeSpeed = 500;
-    }
-
-
-    setTimeout(() => this.type(), typeSpeed)
-}
-
-// Init on DOM load
-document.addEventListener('DOMContentLoaded', init);
-
-// Init App
-function init(){
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-
-    //Init Typewriter
-    new TypeWriiter(txtElement, words, wait);
-}
-
-
 /* Description: Custom JS file */
 
 
@@ -90,13 +20,13 @@ function init(){
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
+			}, 1500, 'easeInOutExpo');
 			event.preventDefault();
 		});
     });
 
     // offcanvas script from Bootstrap + added element to close menu on click in small viewport
-    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle)').on('click', function () {
         $('.top-clicks').toggleClass('open')
     })
 
